5856b89fbf0f378439edcd1e6591347bff1a51f6
Nimeryon <t.auriol46@gmail.com>
2021-04-01 13:36:35 +0200

    countdown

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,6 +38,8 @@ namespace BridgeMonitor.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+
+
         public static List<BoatModel> GetBoats()
         {
             using (var client = new HttpClient())
